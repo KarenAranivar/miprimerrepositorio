17aeb97639c712dcb629a25193c32f02856ef0cb
karan <karan@LAPTOP-HTBLT890>
2020-08-07 10:31:31 -0500

    primer cambio

---
+++ ProyectoBD2/src/proyectobd2/ProyectoBD2.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package proyectobd2;
+
+/**
+ *
+ * @author karan
+ */
+public class ProyectoBD2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+   
+    int x=2;
+    String hola="";
+    System.out.println();
+    
+    }
+    
+}
